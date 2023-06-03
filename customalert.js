3dcc5203454d079044352915b32be19fe63ae7a6
Md Abu Talha Siam <130241399+codertalhabd@users.noreply.github.com>
2023-06-03 22:23:17 +0600

    customalert.js

--- customalert.js
+++ customalert.js
@@ -1,26 +1,4 @@
 const notifications = document.querySelector(".notifications"),
-buttons = document.querySelectorAll(".buttons .btn");
-
-// const toastDetails = {
-//     timer: 5000,
-//     success: {
-//         icon: 'fa-circle-check',
-//         text: 'Success: This is a success toast.',
-//     },
-//     error: {
-//         icon: 'fa-circle-xmark',
-//         text: 'Error: This is an error toast.',
-//     },
-//     warning: {
-//         icon: 'fa-triangle-exclamation',
-//         text: 'Warning: This is a warning toast.',
-//     },
-//     info: {
-//         icon: 'fa-circle-info',
-//         text: 'Info: This is an information toast.',
-//     }
-// }
-
 const removeToast = (toast) => {
     toast.classList.add("hide");
     if(toast.timeoutId) clearTimeout(toast.timeoutId); // Clearing the timeout for the toast
@@ -42,8 +20,3 @@ const Toast = (icon,text,timer) => {
     // Setting a timeout to remove the toast after the specified duration
     toast.timeoutId = setTimeout(() => removeToast(toast), timer);
 }
-
-// Adding a click event listener to each button to create a toast when clicked
-// buttons.forEach(btn => {
-//     btn.addEventListener("click", () => createToast(btn.id));
-// });
