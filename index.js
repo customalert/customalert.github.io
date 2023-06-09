9436d7e32cea6c854e7bfd463ecea5580495883d
Custom Alert <customalertbd@gmail.com>
2023-06-09 23:18:57 +0600

    Add files via upload
    Signed-off-by: Custom Alert <customalertbd@gmail.com>

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-hljs.initHighlightingOnLoad();
+// hljs and hljs-copy Calling
+
+hljs.debugMode();
+hljs.addPlugin(
+  new CopyButtonPlugin({
+    hook: (text, el) => {
+      if (el.dataset.copyright) {
+        return `${text}\n\n${el.dataset.copyright}`;
+      }
+      if (el.dataset.highlight) {
+        el.style.filter = "saturate(5)";
+      }
+      if (el.dataset.replace) {
+        return text
+          .replace("{{name}}", "Alexander Graham Bell")
+          .replace("{{dog}}", "Platypus");
+      }
+      return text;
+    }
+  })
+);
+hljs.highlightAll();
+
+// Sticky Navbar
+
 const header = document.querySelector('header');
 const observer = new IntersectionObserver(
     (entries) => {
       const ent = entries[0];
-      console.log(ent);
       !ent.isIntersecting
         ? header.classList.add("sticky")
         : header.classList.remove("sticky");
@@ -15,6 +38,13 @@ const observer = new IntersectionObserver(
   );
   
   observer.observe(document.querySelector('.main'));
+
+
+  document.getElementById('menu').addEventListener('click', () => {
+    header.querySelector('ul').classList.toggle('active');
+  });
+
+// Getting Current Year For Footer
 
   const d = new Date();
   let year = d.getFullYear();
