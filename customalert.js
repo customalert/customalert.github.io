0763b69d01f5b6c380b4295ea4463e76f99283a7
Custom Alert <customalertbd@gmail.com>
2023-09-19 22:11:52 +0600

    Add files via upload
    Signed-off-by: Custom Alert <customalertbd@gmail.com>

--- customalert.js
+++ customalert.js
@@ -14,11 +14,16 @@
         
 const toast_body = document.createElement("ul");
 (toast_body.className = "notifications"), document.querySelector("body").appendChild(toast_body);
+const header = document.querySelector("header");
+if(header){
+    const h = header.offsetHeight;
+    document.querySelector('.notifications').style.top = h+'px';
+}
 const notifications = document.querySelector(".notifications"),
     removeToast = (t) => {
         t.classList.add("hide"), t.timeoutId && clearTimeout(t.timeoutId), setTimeout(() => t.remove(), 500);
     },
-    Toast = ({ icon: t, text: e, timer: o }) => {
+    Toast = ({ icon: t, text: e, timer: o = 3000 }) => {
         const n = document.createElement("li"),
             i = "tm" + o;
         (n.className = `toast ${t} ${i}`),
@@ -29,12 +34,22 @@ const notifications = document.querySelector(".notifications"),
     removePopup = (t) => {
         t.classList.add("hide"), t.timeoutId && clearTimeout(t.timeoutId), setTimeout(() => t.remove(), 500);
     },
-    Popup = ({ icon: t, title: e, text: o }) => {
+    Popup = ({ icon: t, title: e = "", text: o }) => {
         const n = document.createElement("div");
-        (n.className = "popup_body"),
-            document.querySelector("body").appendChild(n),
-            (n.innerHTML = `\n    <div class="popup">\n    <i class="${t}"></i>\n  <h2 class='popup-title'>\n ${e}\n </h2>\n  <p class="text">\n      ${o}\n    </p>\n    <div class="buttons">\n      <button class="btn ok" id="info">\n        Ok\n      </button>\n    </div>\n  </div>`),
-            n.querySelector(".buttons .ok").addEventListener("click", () => {
-                removePopup(n);
-            });
+            if(e==""){
+                (n.className = "popup_body"),
+                document.querySelector("body").appendChild(n),
+                (n.innerHTML = `\n    <div class="popup">\n    <i class="${t}"></i>\n  <p class="text">\n      ${o}\n    </p>\n    <div class="buttons">\n      <button class="btn ok" id="info">\n        Ok\n      </button>\n    </div>\n  </div>`),
+                n.querySelector(".buttons .ok").addEventListener("click", () => {
+                    removePopup(n);
+                });
+            }else{
+                (n.className = "popup_body"),
+                document.querySelector("body").appendChild(n),
+                (n.innerHTML = `\n    <div class="popup">\n    <i class="${t}"></i>\n  <h2 class='popup-title'>\n ${e}\n </h2>\n  <p class="text">\n      ${o}\n    </p>\n    <div class="buttons">\n      <button class="btn ok" id="info">\n        Ok\n      </button>\n    </div>\n  </div>`),
+                n.querySelector(".buttons .ok").addEventListener("click", () => {
+                    removePopup(n);
+                });
+            }
     };
+
